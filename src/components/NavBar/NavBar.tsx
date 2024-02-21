6d33c8b05eae1b0cea6eb9f54daa49f69b079b78
Daniel <luglifilho@hotmail.com>
2024-02-21 15:27:45 -0300

    inclusao do component temas

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,29 +1,38 @@
-import { Link } from "react-router-dom"
+import { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
 import  './Navbar.css'
 
 function Navbar() {
  
-  
+  const navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent
 
     return (
       <>
-      
-       <div className='w-full bg-sky-600 text-white flex justify-center py-4 navbar'>
-       <div className="imgR"> </div>
-         <div className="container flex justify-between text-lg navbar">
-           <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
-              <div className='flex gap-4'>
-                <Link to='/login' className="hover:underline">Login</Link>
-                <Link to='/home' className="hover:underline"> Home</Link>
-                <div className='hover:underline'>Postagens</div>
-                <div className='hover:underline'>Temas</div>
-                <div className='hover:underline'>Cadastrar tema</div>
-                <div className='hover:underline'>Perfil</div>
-                <div className='hover:underline'>Sair</div>
-              </div>
-            </div>
-          </div>
-      </>
+      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+           <div className="container flex justify-between text-lg">
+           <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+             <div className='flex gap-4'>
+             <div className='hover:underline'>Postagens</div>
+             <Link to='/temas' className='hover:underline'>Temas</Link>
+               <div className='hover:underline'>Cadastrar tema</div>
+               <div className='hover:underline'>Perfil</div>
+               <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+              
+             </div>
+           </div>
+         </div>
+     </>
     )
   }
   
